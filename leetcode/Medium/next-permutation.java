a37a81694d81b52dc66e1ffa76492c9683b2d96d
Sumeet Prajapati <sumeet9704@outlook.com>
2026-09-26 18:56:40 +0530

    Solved: Next Permutation (leetcode)

---
+++ leetcode/Medium/next-permutation.java
@@ -0,0 +1,45 @@
+// Problem: Next Permutation
+// Platform: leetcode
+// Rating/Difficulty: Medium
+// Language: java
+// Verdict: Accepted
+// URL: https://leetcode.com/problems/next-permutation/
+// Solved on: 2026-09-26T13:26:40.085Z
+
+class Solution {
+
+    public void nextPermutation(int[] nums) {
+
+        int i = nums.length - 2;
+
+        while (i >= 0 && nums[i] >= nums[i + 1]) {
+            i--;
+        }
+
+        if (i >= 0) {
+            int j = nums.length - 1;
+
+            while (nums[j] <= nums[i]) {
+                j--;
+            }
+
+            swap(nums, i, j);
+        }
+
+        reverse(nums, i + 1, nums.length - 1);
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    private void reverse(int[] nums, int left, int right) {
+        while (left < right) {
+            swap(nums, left, right);
+            left++;
+            right--;
+        }
+    }
+}
